794f00c66887a4411467757e0914cb0fd56cdd6a
Naman Muley <naman.g.muley@gmail.com>
2025-07-30 02:01:28 +0530

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+    role = Column(String, nullable=False)
+    status = Column(String, nullable=False)
